564649e9439dd9528b8347eec893d64dbfba1a96
Rebecca Ahn <rebeccahn360@gmail.com>
2024-02-21 02:53:14 -0800

    Added pseudocode for adding and getting health data

--- health-habitat/App.js
+++ health-habitat/App.js
@@ -11,11 +11,13 @@ import {
   Dashboard,
 } from './src/screens/login'
 import  { initializeApp } from 'firebase/app'
+import { getFirestore } from "firebase/firestore";
 import { getAuth } from 'firebase/auth'
 import { firebaseConfig } from './src/core/config'
 
 const Stack = createNativeStackNavigator();
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
 export const auth = getAuth(app);
 
 export default function App() {
---
+++ health-habitat/src/api/add-data.js
@@ -0,0 +1,36 @@
+import { db } from '../../App'
+
+// User personal info & tasks
+// Create "User_Info" collection
+
+// Function: In "User_Info" collection, create "user" document with initially empty Diet_, Exercise_, and Meditation_Tasks collections
+
+// Function: In "Diet_Tasks" collection, create "task" document
+
+// Function: In "Exercise_Tasks" collection, create "task" document
+
+// Function: In "Meditation_Tasks" collection, create "task" document
+
+// Function: Update "user" document in "User_Info" collection
+
+// Function: Update "is_completed" field of "task" document in "*_Tasks" collection
+
+
+// User category-based preferences
+// Create "Diet_Preferences" collection
+
+// Create "Exercise_Preferences" collection
+
+// Create "Meditation_Preferences" collection
+
+// Function: In "Diet_Preferences" collection, create "user_preference" document
+
+// Function: Update "user_preference" document in "Diet_Preferences" collection
+
+// Function: In "Exercise_Preferences" collection, create "user_preference" document
+
+// Function: Update "user_preference" document in "Exercise_Preferences" collection
+
+// Function: In "Meditation_Preferences" collection, create "user_preference" document
+
+// Function: Update "user_preference" document in "Meditation_Preferences" collection
