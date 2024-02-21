8b66d6e561ef5047fee9f04d852c53b45c52fd29
Rebecca Ahn <rebeccahn360@gmail.com>
2024-02-21 02:53:14 -0800

    Added pseudocode for adding and getting health data

---
+++ health-habitat/src/api/get-data.js
@@ -0,0 +1,44 @@
+import { db } from '../../App'
+
+// Getting tasks
+// Function: Retrieve all "task" documents from "Diet_Tasks" collection from "user" document from "User_Info" collection
+
+// Function: Retrieve all "task" document from "Exercise_Tasks" collection from "user" document from "User_Info" collection
+
+// Function: Retrieve all "task" documents from "Meditation_Tasks" collection from "user" document from "User_Info" collection
+
+
+// Getting scores
+// Function: Retrieve "score" field from "Diet_Preferences" collection
+
+// Function: Retrieve "score" field from "Exercise_Preferences" collection
+
+// Function: Retrieve "score" field from "Meditation_Preferences" collection
+
+
+// Getting titles (when card is collapsed)
+// Function: Retrieve "title" field from "Diet_Preferences" collection
+
+// Function: Retrieve "title" field from "Exercise_Preferences" collection
+
+// Function: Retrieve "title" field from "Meditation_Preferences" collection
+
+
+// Getting descriptions (when card is expanded)
+// Function: Retrieve "long_description" field from "Diet_Preferences" collection
+
+// Function: Retrieve "long_description" field from "Exercise_Preferences" collection
+
+// Function: Retrieve "long_description" field from "Meditation_Preferences" collection
+
+
+// Getting current data
+// Function: Retrieve "Apple Health Kit" data
+
+// Function: Retrieve "weather" data
+
+// Function: Retrieve "time" data
+
+// Function: Retrieve "location" data
+
+// Function: Retrieve "calendar event" data
