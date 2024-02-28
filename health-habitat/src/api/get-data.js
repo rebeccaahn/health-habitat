f34b6feb5e7b62a056694ada716cc2ea675480a9
Rebecca Ahn <rebeccahn360@gmail.com>
2024-02-27 18:53:34 -0800

    Changed to camelcase

--- health-habitat/src/api/get-data.js
+++ health-habitat/src/api/get-data.js
@@ -1,11 +1,11 @@
 import { db } from '../../App'
 import { collection, query, where, getDocs } from "firebase/firestore";
 
-// Combining queries of Exercise_Tasks to get personalized task(s) because AND queries only work on 1 field at a time
+// Combining queries of ExerciseTasks to get personalized task(s) because AND queries only work on 1 field at a time
 async function getRecExerciseTask(category, muscle, equipment) {
-    const categoryQ = query(collection(db, "Exercise_Tasks"), where("category", "==", category));
-    const muscleQ = query(collection(db, "Exercise_Tasks"), where("muscle_en", "==", muscle));
-    const equipmentQ = query(collection(db, "Exercise_Tasks"), where("equipment", "==", equipment));
+    const categoryQ = query(collection(db, "ExerciseTasks"), where("category", "==", category));
+    const muscleQ = query(collection(db, "ExerciseTasks"), where("muscle_en", "==", muscle));
+    const equipmentQ = query(collection(db, "ExerciseTasks"), where("equipment", "==", equipment));
     
     const categorySnapshot = await getDocs(categoryQ);
     const muscleSnapshot = await getDocs(muscleQ);
@@ -13,40 +13,38 @@ async function getRecExerciseTask(category, muscle, equipment) {
     let exerciseUnion = _.union(categorySnapshot.docs, muscleSnapshot.docs, equipmentSnapshot.docs)   // Uniquely union multiple arrays
     
     let randomExercise = _.sample(exerciseUnion)
-    return randomExercise.get("name")
     // console.log(randomExercise.get("name"))
-    // exerciseUnion.forEach((doc) => {
-    //   console.log(doc.id, " => ", doc.get("name"));
-    // });
+    return randomExercise.get("name")
 }
 
-// Combining queries of Meditation_Tasks to get personalized task(s) because AND queries only work on 1 field at a time
+// Combining queries of MeditationTasks to get personalized task(s) because AND queries only work on 1 field at a time
 async function getRecMeditationTask(tag, duration) {
-    const tagQ = query(collection(db, "Meditation_Tasks"), where("tags", "array-contains", tag));
-    const durationQ = query(collection(db, "Meditation_Tasks"), where("duration", "<=", duration));
+    const tagQ = query(collection(db, "MeditationTasks"), where("tags", "array-contains", tag));
+    const durationQ = query(collection(db, "MeditationTasks"), where("duration", "<=", duration));
     
     const tagSnapshot = await getDocs(tagQ);
     const durationSnapshot = await getDocs(durationQ);
     let meditationUnion = _.union(tagSnapshot.docs, durationSnapshot.docs)   // Uniquely union multiple arrays
     
     let randomMeditation = _.sample(meditationUnion)
-    return randomMeditation.get("url")
     // console.log(randomMeditation.get("url"))
-    // meditationUnion.forEach((doc) => {
-    //   console.log(doc.id, " => ", doc.get("url"));
-    // });
+    return randomMeditation.get("url")
 }
 
-// Calculating score of Exercise_Tasks
+// Calculating score of ExerciseTasks
 async function getExerciseScore() {
-    const exerciseScoreQ = query(collection(db, "Exercise_Tasks"), where("is_completed", "==", false));
+    const exerciseScoreQ = query(collection(db, "ExerciseTasks"), where("isCompleted", "==", false));
     const exerciseScoreSnapshot = await getDocs(exerciseScoreQ);
-    let exercise_score = 100 - exerciseScoreSnapshot.size
+    let exerciseScore = 100 - exerciseScoreSnapshot.size
+    // console.log(exerciseScore)
+    return exerciseScore
 }
 
-// Calculating score of Meditation_Tasks
+// Calculating score of MeditationTasks
 async function getMeditationScore() {
-    const meditationScoreQ = query(collection(db, "Exercise_Tasks"), where("is_completed", "==", false));
+    const meditationScoreQ = query(collection(db, "ExerciseTasks"), where("isCompleted", "==", false));
     const meditationScoreSnapshot = await getDocs(meditationScoreQ);
-    let meditation_score = 100 - meditationScoreSnapshot.size
+    let meditationScore = 100 - meditationScoreSnapshot.size
+    // console.log(meditationScore)
+    return meditationScore
 }
