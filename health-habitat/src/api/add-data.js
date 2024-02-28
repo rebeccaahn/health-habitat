3abde90c4812384d61134f0092c235ac8e6cff6f
Rebecca Ahn <rebeccahn360@gmail.com>
2024-02-28 01:55:50 -0800

    Updated fields

--- health-habitat/src/api/add-data.js
+++ health-habitat/src/api/add-data.js
@@ -2,12 +2,11 @@ import { db } from '../../App'
 import { doc, addDoc } from "firebase/firestore"; 
 
 // Adding document to collection ExerciseTasks
-async function addExerciseTask(title, desc, type, muscle, equip, completed) {
+async function addExerciseTask(title, desc, type, equip, completed) {
     const exerciseTaskData = {
         name: title,
         description: desc,
         category: type,
-        muscleEn: muscle,
         equipment: equip,
         isCompleted: completed
     };
@@ -30,64 +29,24 @@ async function addMeditationTask(title, desc, time, name, genres, completed) {
 }
 
 // Adding document to collection UserInfo
-async function addUser(id, time, weather, time, allergies, priceLimit, time, exerciseType, muscle, equipment, genres, muscleType) {
+async function addUser(id, time, weather, cuisines, diets, allergies, priceLimit, time, exerciseTypes, equipments, genres, time) {
     const userData = {
         userID: id,
-        preferredTime: time,
         preferredWeather: weather,
+        dietCuisines: cuisines,
+        dietDiets: diets,
         dietAllergies: allergies,
         dietPriceLimit: priceLimit,
-        exerciseCategory: exerciseType,
-        exerciseMuscle: muscle,
-        exerciseEquipment: equipment,
-        meditationTag: genre,   // if user prefers multiple tags: meditationTags: genres
+        exerciseCategories: exerciseTypes,
+        exerciseEquipments: equipments,
+        meditationTags: genres,
         duration: time
     };
     
     await addDoc(collection(db, "UserInfo"), userData);
 }
 
-
-// UNNECESSARY TO SEPARATE OUT PREFERENCES IN SUBCOLLECTIONS?
-// // Adding document to subcollection DietPreferences
-// async function addDietPreferences(userID, allergiesArray, price, minutes) {
-//     const dietPreferencesData = {
-//         allergies: allergiesArray,
-//         priceLimit: price,
-//         duration: minutes
-//     };
-    
-//     await addDoc(collection(db, "UserInfo", userID, "DietPreferences"), dietPreferencesData);
-// }
-
-// // Adding document to subcollection ExercisePreferences
-// async function addExercisePreferences(userID, exerciseType, muscleType, equip) {
-//     const exercisePreferencesData = {
-//         category: exerciseType,
-//         muscleEn: muscleType,
-//         equipment: equip
-//     };
-    
-//     await addDoc(collection((db, "UserInfo", userID, "ExercisePreferences"), exercisePreferencesData);
-// }
-
-// // Adding document to subcollection MeditationPreferences
-// async function addMeditationPreferences(userID, genre, milliseconds) {
-//     const meditationPreferencesData = {
-//         tag: genre,
-//         duration: milliseconds
-//     };
-    
-//     await addDoc(collection((db, "UserInfo", userID, "MeditationPreferences"), meditationPreferencesData);
-// }
-
-
+// TODO?
 // Function: Update "isCompleted" field of document in "*Tasks" collection
 
 // Function: Update document in "UserInfo" collection
-
-// Function: Update document in "DietPreferences" collection
-
-// Function: Update document in "ExercisePreferences" collection
-
-// Function: Update document in "MeditationPreferences" collection
--- health-habitat/src/api/task-recommendation.js
+++ health-habitat/src/api/task-recommendation.js
@@ -7,25 +7,25 @@ function recommendDietTask() {
 }
 
 // TODO: Incorporate live data into recommendation factors as well
-// Combining queries of ExerciseTasks to get personalized task(s) because AND queries only work on 1 field at a time
-async function recommendExerciseTask(category, muscle, equipment) {
+// Combining queries of ExerciseTasks to get personalized task(s) 
+// because AND queries only work on 1 field at a time
+async function recommendExerciseTask(category, equipment) {
     const categoryQ = query(collection(db, "ExerciseTasks"), where("category", "==", category));
-    const muscleQ = query(collection(db, "ExerciseTasks"), where("muscleEn", "==", muscle));
     const equipmentQ = query(collection(db, "ExerciseTasks"), where("equipment", "==", equipment));
     
     const categorySnapshot = await getDocs(categoryQ);
-    const muscleSnapshot = await getDocs(muscleQ);
     const equipmentSnapshot = await getDocs(equipmentQ);
     // Uniquely union multiple arrays
-    let exerciseUnion = _.union(categorySnapshot.docs, muscleSnapshot.docs, equipmentSnapshot.docs);
+    let exerciseUnion = _.union(categorySnapshot.docs, equipmentSnapshot.docs);
     
     let randomExercise = _.sample(exerciseUnion);
     return randomExercise.get("name");
 }
 
-// Combining queries of MeditationTasks to get personalized task(s) because AND queries only work on 1 field at a time
+// Combining queries of MeditationTasks to get personalized task(s) 
+// because AND queries only work on 1 field at a time
 async function recommendMeditationTask(tag, duration) {
-    const tagQ = query(collection(db, "MeditationTasks"), where("tags", "array-contains", tag));
+    const tagQ = query(collection(db, "MeditationTasks"), where("meditationTags", "array-contains", tag));
     // const tagsQ = query(collection(db, "MeditationTasks"), where("tags", "in", tags));  // if user prefers multiple tags
     const durationQ = query(collection(db, "MeditationTasks"), where("duration", "<=", duration));
     
