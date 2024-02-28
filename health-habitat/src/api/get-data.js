17f276200db904ef6a740512fd3a0ea936dc48af
Rebecca Ahn <rebeccahn360@gmail.com>
2024-02-27 18:26:20 -0800

    Experimenting with querying the tasks

--- health-habitat/src/api/get-data.js
+++ health-habitat/src/api/get-data.js
@@ -1,44 +1,43 @@
 import { db } from '../../App'
+import { collection, query, where, getDocs } from "firebase/firestore";
 
-// Getting tasks
-// Function: Retrieve all "task" documents from "Diet_Tasks" collection from "user" document from "User_Info" collection
+// Combining queries of Exercise_Tasks to get personalized task(s) because AND queries only work on 1 field at a time
+const categoryQ = query(collection(db, "Exercise_Tasks"), where("category", "==", "Abs"));
+const muscleQ = query(collection(db, "Exercise_Tasks"), where("muscle_en", "==", "Shoulders"));
+const equipmentQ = query(collection(db, "Exercise_Tasks"), where("equipment", "==", "Barbell"));
 
-// Function: Retrieve all "task" document from "Exercise_Tasks" collection from "user" document from "User_Info" collection
+const categorySnapshot = await getDocs(categoryQ);
+const muscleSnapshot = await getDocs(muscleQ);
+const equipmentSnapshot = await getDocs(equipmentQ);
+let exerciseUnion = _.union(categorySnapshot.docs, muscleSnapshot.docs, equipmentSnapshot.docs)   // Uniquely union multiple arrays
 
-// Function: Retrieve all "task" documents from "Meditation_Tasks" collection from "user" document from "User_Info" collection
+let randomExercise = _.sample(exerciseUnion)
+console.log(randomExercise.get("name"))
+// exerciseUnion.forEach((doc) => {
+//   console.log(doc.id, " => ", doc.get("name"));
+// });
 
-
-// Getting scores
-// Function: Retrieve "score" field from "Diet_Preferences" collection
-
-// Function: Retrieve "score" field from "Exercise_Preferences" collection
-
-// Function: Retrieve "score" field from "Meditation_Preferences" collection
-
-
-// Getting titles (when card is collapsed)
-// Function: Retrieve "title" field from "Diet_Preferences" collection
-
-// Function: Retrieve "title" field from "Exercise_Preferences" collection
-
-// Function: Retrieve "title" field from "Meditation_Preferences" collection
-
-
-// Getting descriptions (when card is expanded)
-// Function: Retrieve "long_description" field from "Diet_Preferences" collection
-
-// Function: Retrieve "long_description" field from "Exercise_Preferences" collection
-
-// Function: Retrieve "long_description" field from "Meditation_Preferences" collection
+// Calculating score of Exercise_Tasks
+const exerciseScoreQ = query(collection(db, "Exercise_Tasks"), where("is_completed", "==", false));
+const exerciseScoreSnapshot = await getDocs(exerciseScoreQ);
+let exercise_score = 100 - exerciseScoreSnapshot.size
 
 
-// Getting current data
-// Function: Retrieve "Apple Health Kit" data
+// Combining queries of Meditation_Tasks to get personalized task(s) because AND queries only work on 1 field at a time
+const tagQ = query(collection(db, "Meditation_Tasks"), where("tags", "array-contains", "pop"));
+const durationQ = query(collection(db, "Meditation_Tasks"), where("duration", "<=", "240000"));
 
-// Function: Retrieve "weather" data
+const tagSnapshot = await getDocs(tagQ);
+const durationSnapshot = await getDocs(durationQ);
+let meditationUnion = _.union(tagSnapshot.docs, durationSnapshot.docs)   // Uniquely union multiple arrays
 
-// Function: Retrieve "time" data
+let randomMeditation = _.sample(meditationUnion)
+console.log(randomMeditation.get("url"))
+// meditationUnion.forEach((doc) => {
+//   console.log(doc.id, " => ", doc.get("url"));
+// });
 
-// Function: Retrieve "location" data
-
-// Function: Retrieve "calendar event" data
+// Calculating score of Meditation_Tasks
+const meditationScoreQ = query(collection(db, "Exercise_Tasks"), where("is_completed", "==", false));
+const meditationScoreSnapshot = await getDocs(meditationScoreQ);
+let meditation_score = 100 - meditationScoreSnapshot.size
