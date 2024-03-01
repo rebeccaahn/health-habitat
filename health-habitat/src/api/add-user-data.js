071189bc9f06bb3fe85447db4e4e7cb9d15c31de
Rebecca Ahn <rebeccahn360@gmail.com>
2024-03-01 15:39:30 -0800

    Updated questionnaire data fields

--- health-habitat/src/api/add-user-data.js
+++ health-habitat/src/api/add-user-data.js
@@ -2,27 +2,23 @@ import { db } from '../../App'
 import { collection, addDoc } from "firebase/firestore"; 
 
 // Adding document to collection UserInfo
-export default async function addUser(id, weather, calories, cuisines, diets, allergies, priceLimit, time, exerciseType, genre, time) {
+// TODO: add parameters here
+export default async function addUser() {
     const userData = {
         dietCalories: calories,
         dietDiets: diets,
         dietAllergies: allergies,
-        indoor: 
-
-        userID: id,
+        locationType: location,
+        exerciseTime: exerciseDuration,
+        exerciseCategory: exerciseType,
+        exerciseIntensity: intensity,
+        meditationTime: meditationDuration,
+        meditationTag: genre,
+        userID: email,  // TODO: how to grab userID or email from authentication
         preferredWeather: weather,
         dietCuisines: cuisines,
-        dietPriceLimit: priceLimit,
-        exerciseCategory: exerciseType,
-        // exerciseEquipments: equipments,
-        meditationTag: genre,
-        dietDuration: time
+        dietTime: dietDuration
     };
     
     await addDoc(collection(db, "UserInfo"), userData);
 }
-
-// TODO?
-// Function: Update "isCompleted" field of document in "*Tasks" collection
-
-// Function: Update document in "UserInfo" collection
