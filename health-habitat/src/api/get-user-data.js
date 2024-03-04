f827a1452b040420219b07408fc8b4bdb1074f3c
Rebecca Ahn <rebeccahn360@gmail.com>
2024-03-04 15:59:26 -0800

    Integrated questionnaire into firestore

--- health-habitat/src/api/get-user-data.js
+++ health-habitat/src/api/get-user-data.js
@@ -2,7 +2,7 @@ import { db } from '../../App'
 import { collection, query, where, getDocs } from "firebase/firestore";
 
 // Getting ideal meal type based on current time
-function getMealType()
+export function getMealType()
 {
     const currentHour = Date.now(); // need to find correct function that gives correct hour
 
@@ -29,84 +29,109 @@ function getMealType()
 }
 
 // Finding user's document
-async function getUserDocument(email) {
+export async function getUserDocument(email) {
     const userQ = query(collection(db, "UserInfo"), where("userID", "==", email));
     const userSnapshot = await getDocs(userQ);
     return userSnapshot.docs[0];    // there should only be 1 document in the snapshot as the email is unique
 }
 
 // Getting user's preferred weather
-function getWeather(userDoc) {
+export function getWeather(userDoc) {
     return userDoc.get("preferredWeather");
 }
 
 // Getting user's preferred diet calories
-function getCalories(userDoc) {
+export function getCalories(userDoc) {
     return userDoc.get("dietCalories");
 }
 
 // Getting user's preferred diet cuisines
-function getCuisines(userDoc) {
+export function getCuisines(userDoc) {
     return userDoc.get("dietCuisines");
 }
 
 // Getting user's diet restrictions
-function getRestrictions(userDoc) {
+export function getRestrictions(userDoc) {
     return userDoc.get("dietRestrictions");
 }
 
 // Getting user's diet allergies
-function getAllergies(userDoc) {
+export function getAllergies(userDoc) {
     return userDoc.get("dietAllergies");
 }
 
 // Getting user's preferred diet price limit
-function getPriceLimit(userDoc) {
+export function getPriceLimit(userDoc) {
     return userDoc.get("dietPriceLimit");
 }
 
 // Getting user's diet time
-function getDietTime(userDoc) {
+export function getDietTime(userDoc) {
     return userDoc.get("dietTime");
 }
 
 // Getting user's exercise location type
-function getLocationType(userDoc) {
+export function getLocationType(userDoc) {
     return userDoc.get("exerciseLocation");
 }
 
 // Getting user's exercise time
-function getExerciseTime(userDoc) {
+export function getExerciseTime(userDoc) {
     return userDoc.get("exerciseTime");
 }
 
 // Getting user's preferred exercise categories
-function getCategories(userDoc) {
+export function getCategories(userDoc) {
     return userDoc.get("exerciseCategories");
 }
 
 // Getting user's preferred exercise equipments
-function getEquipments(userDoc) {
+export function getEquipments(userDoc) {
     return userDoc.get("exerciseEquipments");
 }
 
 // Getting user's preferred exercise intensity
-function getIntensity(userDoc) {
+export function getIntensity(userDoc) {
     return userDoc.get("exerciseIntensity");
 }
 
 // Getting user's preferred meditation tags
-function getTags(userDoc) {
+export function getTags(userDoc) {
     return userDoc.get("meditationTags");
 }
 
-// Getting user's preferred meditation duration in milliseconds
-function getMeditationTime(userDoc) {
+// Getting user's preferred meditation duration in seconds
+export function getMeditationTime(userDoc) {
     time = userDoc.get("meditationTime")
-    return time*60000;
+    return time*60;
+}
+
+export function getDietScore(userDoc) {
+    return userDoc.get("dietScore");
+}
+
+export function getExerciseScore(userDoc) {
+    return userDoc.get("exerciseScore");
+}
+
+export function getMeditationScore(userDoc) {
+    return userDoc.get("meditationScore");
+}
+
+// [id, timestamp]
+export function getDietTask(userDoc) {
+    return userDoc.get("dietTask");
+}
+
+export function getExerciseTask(userDoc) {
+    return userDoc.get("exerciseTask");
+}
+
+export function getMeditationTask(userDoc) {
+    return userDoc.get("meditationTask");
 }
 
 // TODO: Getting user's current data
-function get() {
+export function get() {
     return;
 }
