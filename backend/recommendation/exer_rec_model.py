f5e64a2c53bec0e8a8c08a23de97695f6a84b99d
Teresa Liang <teresaliang2003@gmail.com>
2024-03-11 22:12:01 -0700

    implemented more sophisticated exercise ml model

--- backend/recommendation/exer_rec_model.py
+++ backend/recommendation/exer_rec_model.py
@@ -1,92 +1,185 @@
-from pathlib import Path
-from typing import List
+'''
+Module to recommend exercise task based on age, dream weight, actual weight, duration, heart rate, BMI, gender, weather conditions, and weight difference
+Based off of model in https://www.kaggle.com/datasets/aakashjoshi123/exercise-and-fitness-metrics-dataset
+'''
+
 import pandas as pd
-from sklearn.tree import DecisionTreeClassifier
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import phik
+from sklearn.model_selection import train_test_split, cross_val_score, RandomizedSearchCV, GridSearchCV
+from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import OrdinalEncoder, Normalizer
+from sklearn.impute import SimpleImputer
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import GradientBoostingRegressor
 import random
-
-
+from pathlib import Path
+from typing import List, Tuple
 class ExerciseClassifier:
     """
     Builds a Decision Tree Classifier model based off of training data input through CSV file
     """
+    cat_features = [
+            'Exercise', 
+            'Gender', 
+            'Weather Conditions', 
+            'Gain'
+    ]
 
-    """Maps for encoding data"""
-    group_map = {'abs': 0, 'arms': 1, 'back': 2, 'calves': 3, 'cardio': 4, 'chest': 5, 'legs': 6, 'shoulders': 7}
-    group_reverse_map = {v: k for k, v in group_map.items()}
-    heart_rate_map = {'low': 0, 'moderate': 1, 'high': 2}
-    
+    num_features = [
+        'Dream Weight', 
+        'Actual Weight', 
+        'Age', 
+        'Duration', 
+        'Heart Rate', 
+        'BMI', 
+        'Weight Difference'
+    ]
 
-    def __init__(self, training_data_path: Path):
+    target = 'Exercise Intensity'
+
+    def __init__(self, data_path: Path):
         """
         Reccomendation model using a Decision Tree Classifier
 
         Imports training data from a CSV file and encodes it for model training
 
         Parameters:
-            training_data_path (Path): Path to the CSV file containing the training data
+            data_path (Path): Path to the CSV file containing the training data
         """
-        self._training_data = self._extract_training_data(training_data_path)
-        self._encode_training_data()
-        self._model = None
+        self.state = np.random.RandomState(12345)
+        print(data_path)
+        self._data = self._extract_data(data_path)
+        self.trainX, self.testX, self.trainY, self.testY = self._split_data(self._data)
+        # model based on the best hyperparameters found from https://www.kaggle.com/datasets/aakashjoshi123/exercise-and-fitness-metrics-dataset
+        self._model = GradientBoostingRegressor(
+            loss='absolute_error',
+            max_depth=1,
+            max_features='log2',
+            min_samples_leaf=2,
+            min_samples_split=8,
+            n_estimators=50
+            )
+        
+        # pipeline
+        numeric_transformer = Pipeline(
+            steps=[
+                ('imputer', SimpleImputer(strategy='mean')),
+                ('normalizer', Normalizer())
+            ]
+        )
 
-    def _extract_training_data(self, training_data_path: Path) -> List[List[str | int]]:
+        categorical_transformer = Pipeline(
+            steps=[
+                ('imputer', SimpleImputer(strategy='constant')),
+                ('encoder', OrdinalEncoder(handle_unknown='use_encoded_value', unknown_value=-1))
+            ]
+        )
+
+        self.preprocessor = ColumnTransformer(
+            transformers=[
+                ('numeric', numeric_transformer, self.num_features),
+                ('categorical', categorical_transformer, self.cat_features)
+            ]
+        )
+
+    def _gain(self, x):
+        if x < 0:
+            return 'Gain'
+        else: return 'Lose'
+
+    def _extract_data(self, data_path: Path) -> List[List[str | int]]:
         """
         Extract the training data from the CSV file
 
         Parameters:
-            training_data_path (Path): Path to the CSV file containing the training data
+            data_path (Path): Path to the CSV file containing the training data
 
         Returns:
             List[List[str | int]]: The training data
         """
         # Read the CSV file
-        training_data = pd.read_csv(training_data_path)
+        data = pd.read_csv(data_path)
+
+        # print data to check if it was read correctly
+        print(data.head())
+        print(data.info())
+
+        # process the training data
+        data.drop('ID', axis=1, inplace=True)
+        data['Exercise'] = data['Exercise'].map(lambda x: ''.join([i for i in x if i.isdigit()])) # change exercise column to int
+        ints = [
+            'Exercise', 
+            'Age', 
+            'Duration', 
+            'Heart Rate', 
+            'Exercise Intensity'
+        ]
+
+        categories = [
+            'Gender', 
+            'Weather Conditions'
+        ]
+
+        for col in ints:
+            data[col] = data[col].astype('int16')
+
+        for col in categories:
+            data[col] = data[col].astype('category')
+
+        # add gain column to describe whether the respondent wants to gain some weight or lose it
+        data['Weight Difference'] = data['Actual Weight'] - data['Dream Weight']
+        data['Gain'] = data['Weight Difference'].apply(self._gain).astype('category')
+        data['Weight Difference'] = abs(data['Weight Difference'])
+
+        # print data to check if it was processed correctly
+        print(data.describe())
 
         # Convert the data to a list of lists
-        training_data = training_data.values.tolist()
+        # data = data.values.tolist()
 
-        return training_data
+        return data
     
-    def _encode_training_data(self):
+    def _split_data(self, data: List[List[str | int]]) -> Tuple[List[List[str | int]], List[List[str | int]], List[str | int], List[str | int]]:
         """
-        Encodes training data into integers for model training
-
-        Utalizes condition_map, time_of_day_map, and genre_map to encode the data
-        """
-        training_data = [[float(self.group_map[data[0].lower()]), float(self.heart_rate_map[data[1].lower()]), 
-                          float(data[2]), float(data[3])]
-                          for data in self._training_data]
-
-        self._training_data = training_data
-    
-    def update_training_data(self, training_data_path: Path):
-        """
-        Update the training data for the model
+        Split the data into training and testing data
 
         Parameters:
-            training_data_path (Path): Path to the new training data
+            data (List[List[str | int]]): The data to split
+
+        Returns:
+            (List[List[str | int]], List[List[str | int]]): The training and testing data
         """
-        self.__init__(training_data_path)
+        X_train, X_test, y_train, y_test = train_test_split(
+            data[self.cat_features + self.num_features], 
+            data[self.target], 
+            test_size=0.33, 
+            random_state=self.state
+            )
+        
+        return X_train, X_test, y_train, y_test
 
     def train_model(self):
         """
         Train the model using the training data
         """
-        # Create columns
-        columns = ["Muscle Group","Heart Rate", "Steps", "Calories" ]
-        df_train = pd.DataFrame(self._training_data, columns=columns)
+        steps = [
+            ('preprocess', self.preprocessor),
+            ('reg', self._model)
+        ]
 
-        # Assign x to input data
-        x_train = df_train[['Heart Rate', 'Steps', 'Calories']]
-        # Assign y to output data
-        y_train = df_train['Muscle Group']
-        print(df_train)
+        pipeline = Pipeline(steps)
 
-        # Create and train Decision Tree Classifier model
-        self._model = DecisionTreeClassifier()
-        self._model.fit(x_train, y_train)
+        # fit the model
+        pipeline.fit(self.trainX, self.trainY)
+
+        self._model = pipeline
+
     
-    def predict_category(self, steps: int, calories: int | float, heart_rate: int | float) -> str:
+    def predict_category(self, dream_weight: int, actual_weight: int, age: int, gender: str, weather_conditions) -> str:
         """
         Predict the category of exercise to do based on the given parameters
 
@@ -103,17 +196,37 @@ class ExerciseClassifier:
             self.train_model()
         
         # Create a new user dataframe
-        new_user = pd.DataFrame([[heart_rate, steps, calories]], columns=['Heart Rate', 'Steps', 'Calories', ])
+        new_user = pd.DataFrame({
+            'Exercise': None,
+            'Calories': None,
+            'Dream Weight': [dream_weight],
+            'Actual Weight': [actual_weight],
+            'Age': [age],
+            'Gender': [gender],
+            'Duration': None,
+            'Heart Rate': None,
+            'BMI': None,
+            'Weather Conditions': [weather_conditions],
+            'Weight Difference': [actual_weight - dream_weight],
+            'Gain': self._gain(actual_weight - dream_weight)
+        })
+
+        # new_user = self.preprocessor.transform(new_user)
+
+
+        # print out new user as a row in dataframe
+        print(new_user)
+
         # Predict the category
         category = self._model.predict(new_user)
 
-        return self.group_reverse_map[int(category[0])]
+        return category[0]
     
     @staticmethod
     def choose_location(weather_condition: str, temperature: int | float, time_of_day: str) -> str:
         """
         Chooses a location to meditate based on the given parameters
-        Locations: home, library, park, beach, cafe
+        Locations: home (inside), backyard, park, gym
 
         Parameters:
             weather_condition (str): The current weather condition
@@ -121,8 +234,10 @@ class ExerciseClassifier:
             time_of_day (str): The current time of day
 
         Returns:
-            str: The location to meditate at
+            str: The location to exercise at
         """
+        # TODO: add gym as a place if user has access to a gym
+
         weather_condition = weather_condition.lower()
         time_of_day = time_of_day.lower()
         temperature = float(temperature)
@@ -138,6 +253,7 @@ class ExerciseClassifier:
         return "home (inside)"
 
 if __name__ == '__main__':
-    exercise_model = ExerciseClassifier("exercise_data.csv")
+    exercise_model = ExerciseClassifier("exercise_dataset.csv")
     exercise_model.train_model()
-    print(exercise_model.predict_category(100, 100, 100)) # Output: 'Cardio'
+    print('RESULT')
+    print(exercise_model.predict_category(91, 96, 45, 'Male', 'Sunny')) # Output: 'Cardio'
