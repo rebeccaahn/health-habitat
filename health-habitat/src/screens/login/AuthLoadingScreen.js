78b3eacee7a961844b33d107f5d7b1b2e7a3d889
Teresa Liang <teresaliang2003@gmail.com>
2024-03-13 22:41:42 -0700

    fixed auth bugs

--- health-habitat/src/screens/login/AuthLoadingScreen.js
+++ health-habitat/src/screens/login/AuthLoadingScreen.js
@@ -1,18 +1,31 @@
 import React from 'react'
 import { ActivityIndicator } from 'react-native'
-import { getAuth } from "firebase/auth";
 import Background from '../../components/Background'
+import { getUserDocument } from '../../api/get-user-data'
 import { theme } from '../../core/theme'
 import { auth } from '../../core/config';
 
 export default function AuthLoadingScreen({ navigation }) {
   auth.onAuthStateChanged((user) => {
+    console.log('user:', user);
     if (user) {
       // User is logged in
-      navigation.reset({
-        index: 0,
-        routes: [{ name: 'TerrariumScreen' }],
-      })
+      try {
+        getUserDocument(auth.currentUser.email).then((value) => {
+          console.log('value', value.data());
+          // user has completed questionnaire
+          navigation.reset({
+            index: 0,
+            routes: [{ name: 'TerrariumScreen' }],
+          })
+        });
+      } catch (error) {
+        // user has not completed questionnaire yet
+        navigation.reset({
+          index: 0,
+          routes: [{ name: 'QuestionnaireScreen' }],
+        })
+      }
     } else {
       // User is not logged in
       navigation.reset({
